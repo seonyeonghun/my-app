01f42397f5a6de20733f434085e7d04d0f5f0aa1
seonyeonghun <sprax@naver.com>
2023-01-19 13:11:21 +0900

    unit converter finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // 화살함수
+  const convert = () => console.log("단위를 min에서 hours로 변경합니다");
+  const [amount, setAmount] = useState("0");
+
+  // 일반함수
+  function handleChange(event) {
+    setAmount(event.target.value);
+  }
+
+  // 화살표 함수 표기법
+  const resetInput = () => setAmount("0");
+  const handleSubmit = (e) => e.preventDefault(); // 이벤트 전파를 막아서 새로고침을 안되게!
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1 className='site-header'>단위 환산 앱</h1>
+      <div className='unit-converter'>
+        <div className='ad'>
+          구글 광고나 네이버 광고 이미지가 옵니다. 광고주님들, 연락주세요 ㅠ
+        </div>
+        <div className='converter'>
+          <form onSubmit={handleSubmit}>
+            <ul>
+              <li>
+                <input
+                  type='text'
+                  value={amount}
+                  onChange={handleChange}
+                  placeholder='값을 입력하세요'
+                />
+                <label>minutes</label>
+              </li>
+              <li>
+                <input
+                  type='text'
+                  value={amount / 60}
+                  placeholder='값을 입력하세요'
+                />
+                <label>hours</label>
+              </li>
+              <li>
+                <button onClick={resetInput}>리셋</button>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
