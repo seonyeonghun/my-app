372fe0449475475ca04c25091a1372cb1c15b5ea
seonyeonghun <sprax@naver.com>
2023-01-26 14:15:46 +0900

    환산앱 최종 버전완료 및 배포 설정/적용

--- src/App.js
+++ src/App.js
@@ -1,60 +1,52 @@
 import React, { useState } from "react";
 import "./App.css";
+import MinutesToHours from "./component/MinutesToHours";
+import MilesToKilometers from "./component/MilesToKilometers";
+import InchToCentimeters from "./component/InchToCentimeters";
 
 function App() {
   // 화살함수
-  const convert = () => console.log("단위를 min에서 hours로 변경합니다");
-  const [amount, setAmount] = useState("0");
-  const [disabled, setDisabled] = useState(false); // 입력폼의 disabled 상태와 연결(=데이터 바인딩)
-  // 일반함수
-  function handleChange(event) {
-    setAmount(event.target.value);
-  }
-
-  // 화살표 함수 표기법
-  const resetInput = () => {
-    setAmount("0");
-    setDisabled(false);
-  };
+  const [mode, setMode] = useState(false);
+  const [converter, setConverter] = useState(0);
+  const handleChange = (e) => setConverter(e.target.value);
   const handleSubmit = (e) => e.preventDefault(); // 이벤트 전파를 막아서 새로고침을 안되게!
-  const inversion = () => {
-    setDisabled((prev) => !prev);
-  };
+  const handleMode = () => setMode((prev) => !prev);
   return (
-    <div className='main'>
-      <h1 className='site-header'>단위 환산 앱</h1>
+    <div className={mode ? "main dark" : "main day"}>
+      <h1 className='site-header'>
+        단위 환산 앱
+        <button onClick={handleMode}>{mode ? "day" : "night"}</button>
+      </h1>
       <div className='unit-converter'>
         <div className='ad'>
           구글 광고나 네이버 광고 이미지가 옵니다. 광고주님들, 연락주세요 ㅠ
+          <p>010-123-4567 / address@yourmail.com</p>
         </div>
         <div className='converter'>
+          <div className='title'>
+            <h3>환산 하고자 하는 작업을 선택하세요!</h3>
+            <select
+              name='set-converter'
+              id='set-converter'
+              onChange={handleChange}
+              value={converter}
+            >
+              <option value='0'>:: 단위변환 ::</option>
+              <option value='1'>Minutes to Hours</option>
+              <option value='2'>Miles to Kilometers</option>
+              <option value='3'>Inch to Centimeter</option>
+            </select>
+          </div>
           <form onSubmit={handleSubmit}>
-            <ul>
-              <li>
-                <input
-                  type='text'
-                  value={disabled ? amount * 60 : amount}
-                  onChange={handleChange}
-                  placeholder='값을 입력하세요'
-                  disabled={disabled}
-                />
-                <label>minutes</label>
-              </li>
-              <li>
-                <input
-                  type='text'
-                  value={!disabled ? amount / 60 : amount}
-                  placeholder='값을 입력하세요'
-                  onChange={handleChange}
-                  disabled={!disabled}
-                />
-                <label>hours</label>
-              </li>
-              <li>
-                <button onClick={resetInput}>리셋</button>
-                <button onClick={inversion}>반전</button>
-              </li>
-            </ul>
+            {converter === "0" ? (
+              <p>
+                단위 환산앱은 분-초, 마일-킬로미터, 인치-센티미터 단위를
+                환산하는데 도움을 드립니다.
+              </p>
+            ) : null}
+            {converter === "1" ? <MinutesToHours /> : null}
+            {converter === "2" ? <MilesToKilometers /> : null}
+            {converter === "3" ? <InchToCentimeters /> : null}
           </form>
         </div>
       </div>
---
+++ src/component/InchToCentimeters.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function MinutesToHours() {
+  const [amount, setAmount] = useState("0");
+  const [disabled, setDisabled] = useState(false); // 입력폼의 disabled 상태와 연결(=데이터 바인딩)
+  // 일반함수
+  function handleChange(event) {
+    setAmount(event.target.value);
+  }
+  // 화살표 함수 표기법
+  const resetInput = () => {
+    setAmount("0");
+    setDisabled(false);
+  };
+  const inversion = () => {
+    setDisabled((prev) => !prev);
+  };
+  return (
+    <>
+      <ul>
+        <li>
+          <input
+            type='text'
+            value={disabled ? amount / 2.54 : amount}
+            onChange={handleChange}
+            placeholder='값을 입력하세요'
+            disabled={disabled}
+          />
+          <label>inch</label>
+        </li>
+        <li>
+          <input
+            type='text'
+            value={!disabled ? amount * 2.54 : amount}
+            placeholder='값을 입력하세요'
+            onChange={handleChange}
+            disabled={!disabled}
+          />
+          <label>centimeters</label>
+        </li>
+        <li>
+          <button onClick={resetInput}>리셋</button>
+          <button onClick={inversion}>반전</button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default MinutesToHours;
